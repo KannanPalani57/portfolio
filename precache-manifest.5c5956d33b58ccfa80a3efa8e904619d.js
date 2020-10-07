b3b4333902cc1af54cd7ce7fd7f7ca3c619fe67c
root <kannanpalani455@gmail.com>
2020-10-07 08:30:49 +0530

    Updates

---
+++ precache-manifest.5c5956d33b58ccfa80a3efa8e904619d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c88c552890f3d0f4fce6b2ca6b33fa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8f0ff37f7823ee033bd4",
+    "url": "/portfolio/static/css/main.9e1480b8.chunk.css"
+  },
+  {
+    "revision": "dc8cf53d50ea50f19223",
+    "url": "/portfolio/static/js/2.d0c7e472.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.d0c7e472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0ff37f7823ee033bd4",
+    "url": "/portfolio/static/js/main.6053cf57.chunk.js"
+  },
+  {
+    "revision": "eb0aafffcb038f123bf0",
+    "url": "/portfolio/static/js/runtime-main.255623fe.js"
+  },
+  {
+    "revision": "ce9b200daa51aac0489c5827587da778",
+    "url": "/portfolio/static/media/kannan.ce9b200d.jpg"
+  },
+  {
+    "revision": "5be6f8216fad0adde289c23cddad7675",
+    "url": "/portfolio/static/media/shareyourbyte.5be6f821.jpg"
+  },
+  {
+    "revision": "0ff958ed6b1fe978e8ef87863d5ad6e9",
+    "url": "/portfolio/static/media/wikipediasearch.0ff958ed.jpg"
+  }
+]);
